a6dc55c4c0722d35988c1b48aa926b206b7e7ff1
Phuc <hphuc4499@gmail.com>
2021-04-18 17:57:28 +0700

    [HP} Frontal face

--- ailibs/utilities/__init__.py
+++ ailibs/utilities/__init__.py
@@ -3,6 +3,6 @@ import sys
 
 
 PYTHON_PATH = os.path.abspath(os.path.join(
-    os.path.abspath(__file__), "..", "..", "..", ".."))
+    os.path.abspath(__file__), "..", "..", ".."))
 os.chdir(PYTHON_PATH)
 sys.path.insert(0, PYTHON_PATH)
