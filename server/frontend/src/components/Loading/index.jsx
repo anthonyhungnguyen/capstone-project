d0f862a6efd1f871be1fd23682a86f363fa29276
Nguyen <anthonyhungnguyen276@gmail.com>
2021-04-27 00:38:45 +0700

    [PH] add loading animation

--- server/frontend/src/components/Loading/index.jsx
+++ server/frontend/src/components/Loading/index.jsx
@@ -1,5 +1,16 @@
 import React from "react"
+import { css } from "@emotion/core"
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader"
 
 export default function Loading() {
-  return <div>loading...</div>
+  const override = css`
+    display: block;
+    margin: auto;
+    border-color: red;
+  `
+  return (
+    <div className="h-screen flex items-center">
+      <ClimbingBoxLoader loading={true} size={30} css={override} />
+    </div>
+  )
 }
